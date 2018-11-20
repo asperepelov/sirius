1bcbc4cc2167821bf254852ea7143de97b044d9b
Vladimir Rodionov <vladimirarodionov@gmail.com>
2018-11-20 11:52:27 +0300

    initial authentication version

---
+++ SiriusCRM/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+#from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+    )
+# Create your views here.
